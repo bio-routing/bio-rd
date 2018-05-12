140a78a3feb38a68ea0623200af1a2afe6692131
takt <oliver.herms@exaring.de>
2018-05-12 16:40:03 +0200

    Update neighbor.go

--- routingtable/neighbor.go
+++ routingtable/neighbor.go
@@ -5,6 +5,6 @@ type Neighbor struct {
 	// Addres is the IPv4 address of the neighbor as integer representation
 	Address uint32
 
-	// Type is the type / protocol used to for routing inforation communitation
+	// Type is the type / protocol used for routing inforation communitation
 	Type uint8
 }
