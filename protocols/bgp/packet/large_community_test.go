c3e245ca6b2261d399a17503ec8885594fc12843
Christoph Petrausch <christoph.petrausch@inovex.de>
2018-06-19 19:54:10 +0200

    Fixed typos

--- protocols/bgp/packet/large_community_test.go
+++ protocols/bgp/packet/large_community_test.go
@@ -58,19 +58,19 @@ func TestParseLargeCommunityString(t *testing.T) {
 			err:      errors.New("malformed large community string (,2,3)"),
 		},
 		{
-			name:     "to big global administrator",
+			name:     "too big global administrator",
 			in:       fmt.Sprintf("(%d,1,2)", math.MaxInt64),
 			expected: LargeCommunity{},
 			err:      &strconv.NumError{Func: "ParseUint", Num: fmt.Sprintf("%d", math.MaxInt64), Err: strconv.ErrRange},
 		},
 		{
-			name:     "to big data part 1",
+			name:     "too big data part 1",
 			in:       fmt.Sprintf("(1,%d,2)", math.MaxInt64),
 			expected: LargeCommunity{1, 0, 0},
 			err:      &strconv.NumError{Func: "ParseUint", Num: fmt.Sprintf("%d", math.MaxInt64), Err: strconv.ErrRange},
 		},
 		{
-			name:     "to big data part 2",
+			name:     "too big data part 2",
 			in:       fmt.Sprintf("(1,2,%d)", math.MaxInt64),
 			expected: LargeCommunity{1, 2, 0},
 			err:      &strconv.NumError{Func: "ParseUint", Num: fmt.Sprintf("%d", math.MaxInt64), Err: strconv.ErrRange},
