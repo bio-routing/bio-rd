37e28661612b30bb70a3daa135806a37e57c9a20
Oliver Herms <oliver.herms@exaring.de>
2018-06-27 17:03:47 +0100

    Fixing comment

--- protocols/bgp/types/options.go
+++ protocols/bgp/types/options.go
@@ -1,6 +1,6 @@
 package types
 
-// Options represents options to the update sender
+// Options represents options to the update sender, decoder and encoder
 type Options struct {
 	Supports4OctetASN bool
 	AddPathRX         bool
