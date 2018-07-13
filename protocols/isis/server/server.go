130f9c0916724916acd36fa03c800bcac3cb7141
takt <takt@takt.guru>
2018-07-13 13:00:07 +0200

    Added packet processing

--- protocols/isis/server/server.go
+++ protocols/isis/server/server.go
@@ -1,15 +1,27 @@
 package server
 
 import (
+	"time"
 	"fmt"
-	"os"
+	"net"
 	"syscall"
 
 	"github.com/bio-routing/bio-rd/config"
+
+	"github.com/mdlayher/raw"
+)
+
+const (
+	maxEtherFrameSize = 9216
 )
 
 type ISISServer struct {
 	config config.ISISConfig
+}
+
+type isisInterface struct {
+	name string
+	rx chan []byte
 }
 
 func NewISISServer(cfg config.ISISConfig) *ISISServer {
@@ -18,33 +30,51 @@ func NewISISServer(cfg config.ISISConfig) *ISISServer {
 	}
 }
 
+func newISISInterface(name string) *isisInterface {
+	return &isisInterface{
+		name: name,
+		rx: make(chan []byte),
+	}
+}
+
 func (isis *ISISServer) Start() error {
-	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_DGRAM, syscall.ETH_P_ALL)
-	if err != nil {
-		return fmt.Errorf("Unable to open socket: %v", err)
-	}
-
-	err = syscall.Bind(fd, syscall.RawSockaddrLinklayer{
-		Protocol: ETH_P_ALL,
-		Family:   AF_PACKET,
-	})
-	if err != nil {
-		return fmt.Errorf("Unable to bind: %v", err)
-	}
-
-	f := os.NewFile(uintptr(fd), fmt.Sprintf("fd %d", fd))
-
-	go func() {
-		for {
-			buffer := make([]byte, 1500)
-			_, err := f.Read(buffer)
-			if err != nil {
-				panic(fmt.Sprintf("Unable to read from socket: %v", err))
-			}
-
-			fmt.Printf("Packet: %v\n", buffer)
+	for _, ifs := range isis.config.Interfaces {
+		ifa, err := net.InterfaceByName(ifs.Name)
+		if err != nil {
+			return fmt.Errorf("Unable to get interface: %v", err)
 		}
-	}()
+
+		c, err := raw.ListenPacket(ifa, syscall.ETH_P_802_2, nil)
+		if err != nil {
+			return fmt.Errorf("Unable to listen: %v", err)
+		}
+
+		c.SetPromiscuous(true)
+
+		go func() {
+			buffer := make([]byte, maxEtherFrameSize)
+			for {
+				n, addr, err := c.ReadFrom(buffer)
+				if err != nil {
+					panic(fmt.Sprintf("Unable to read from socket: %v", err))
+				}
+
+				fmt.Printf("Packet from %v: %v\n", addr.String(), buffer[:n])
+			}
+		}()
+
+		go func() {
+			for {
+				c.WriteTo([]byte{1, 2, 3, 4}, net.HardwareAddr([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}))
+				time.Sleep(time.Second)
+			}
+		}()
+	}
+
+	return nil
+}
+
+func (isis *ISISServer) Stop() error {
 
 	return nil
 }
