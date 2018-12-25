f33beda4e4a3fb087477bc724f9c3eb6fbe2210a
Oliver Herms <oliver.herms@exaring.de>
2018-12-25 19:05:06 +0100

    Adding example

---
+++ examples/device/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bio-routing/bio-rd/protocols/device"
+)
+
+type Client struct {
+}
+
+func (c *Client) LinkUpdate(lu device.LinkUpdate) {
+	fmt.Printf("Link Update! %s\n", lu.Name)
+}
+
+func main() {
+	s := device.New()
+	s.Start()
+
+	c := &Client{}
+	s.Subscribe(c, "virbr0")
+}
