16862699ee206423fb9eacf4e95f9822c096fa40
Oliver Herms <oliver.herms@exaring.de>
2018-12-27 21:01:17 +0100

    Fixes

--- protocols/device/device_linux.go
+++ protocols/device/device_linux.go
@@ -6,17 +6,17 @@ func (d *Device) updateLink(attrs *netlink.LinkAttrs) {
 	d.l.Lock()
 	defer d.l.Unlock()
 
-	d.MTU = attrs.MTU
+	d.MTU = uint16(attrs.MTU)
 	d.Name = attrs.Name
 	copy(d.HardwareAddr, attrs.HardwareAddr)
 	d.Flags = attrs.Flags
-	d.OperState = attrs.OperState
+	d.OperState = uint8(attrs.OperState)
 }
 
 func (d *Device) notify(clients []Client) {
 	n := d.copy()
 
 	for _, c := range clients {
-		c.LinkUpdate(n)
+		c.DeviceUpdate(n)
 	}
 }
