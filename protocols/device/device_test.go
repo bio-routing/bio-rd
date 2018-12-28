29d2489131d8c76cc7549c4fdb8f81b876515c0a
takt <oliver.peter.herms@gmail.com>
2018-12-28 17:34:53 +0100

    Protocol Device (#147)

---
+++ protocols/device/device_test.go
@@ -0,0 +1,117 @@
+package device
+
+import (
+	"testing"
+
+	bnet "github.com/bio-routing/bio-rd/net"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeviceCopy(t *testing.T) {
+	tests := []struct {
+		name     string
+		dev      *Device
+		expected *Device
+	}{
+		{
+			name: "Test #1",
+			dev: &Device{
+				Name: "Foo",
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+				},
+			},
+			expected: &Device{
+				Name: "Foo",
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		copy := test.dev.copy()
+		test.dev.addAddr(bnet.NewPfx(bnet.IPv4(200), 8))
+		assert.Equalf(t, test.expected, copy, "Test %q", test.name)
+	}
+}
+
+func TestDeviceDelAddr(t *testing.T) {
+	tests := []struct {
+		name     string
+		dev      *Device
+		delete   bnet.Prefix
+		expected *Device
+	}{
+		{
+			name: "Test #1",
+			dev: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+					bnet.NewPfx(bnet.IPv4(200), 8),
+					bnet.NewPfx(bnet.IPv4(300), 8),
+				},
+			},
+			delete: bnet.NewPfx(bnet.IPv4(200), 8),
+			expected: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+					bnet.NewPfx(bnet.IPv4(300), 8),
+				},
+			},
+		},
+		{
+			name: "Test #2",
+			dev: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+					bnet.NewPfx(bnet.IPv4(200), 8),
+					bnet.NewPfx(bnet.IPv4(300), 8),
+				},
+			},
+			delete: bnet.NewPfx(bnet.IPv4(100), 8),
+			expected: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(200), 8),
+					bnet.NewPfx(bnet.IPv4(300), 8),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		test.dev.delAddr(test.delete)
+		assert.Equalf(t, test.expected, test.dev, "Test %q", test.name)
+	}
+}
+
+func TestDeviceAddAddr(t *testing.T) {
+	tests := []struct {
+		name     string
+		dev      *Device
+		input    bnet.Prefix
+		expected *Device
+	}{
+		{
+			name: "Test #1",
+			dev: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+				},
+			},
+			input: bnet.NewPfx(bnet.IPv4(200), 8),
+			expected: &Device{
+				Addrs: []bnet.Prefix{
+					bnet.NewPfx(bnet.IPv4(100), 8),
+					bnet.NewPfx(bnet.IPv4(200), 8),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		test.dev.addAddr(test.input)
+		assert.Equalf(t, test.expected, test.dev, "Test %q", test.name)
+	}
+}
---
+++ protocols/device/server_darwin.go
@@ -0,0 +1,14 @@
+package device
+
+import "fmt"
+
+type osAdapterDarwin struct {
+}
+
+func newOSAdapterDarwin(srv *Server) (*osAdapterDarwin, error) {
+	return nil, nil
+}
+
+func (o *osAdapterDarwin) start() error {
+	return fmt.Errorf("Not implemented")
+}
