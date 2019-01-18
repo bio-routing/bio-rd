9649189f886e324e15cdee112e957058ec37b220
Daniel Czerwonk <daniel@dan-nrw.de>
2019-01-18 20:38:16 +0100

    increased test coverage of decoder

--- util/decoder/decoder_test.go
+++ util/decoder/decoder_test.go
@@ -8,34 +8,68 @@ import (
 )
 
 func TestDecode(t *testing.T) {
-	input := []byte{
-		3, 0, 0, 0, 100, 200,
-	}
-
 	type testData struct {
 		a uint8
 		b uint32
 		c []byte
 	}
 
-	s := testData{
-		c: make([]byte, 1),
+	tests := []struct {
+		name     string
+		input    []byte
+		expected testData
+		wantFail bool
+	}{
+		{
+			name: "valid input",
+			input: []byte{
+				3, 0, 0, 0, 100, 200,
+			},
+			expected: testData{
+				a: 3,
+				b: 100,
+				c: []byte{200},
+			},
+		},
+		{
+			name: "input too short",
+			input: []byte{
+				3, 0, 0, 0, 100,
+			},
+			wantFail: true,
+		},
+		{
+			name:     "input null",
+			wantFail: true,
+		},
 	}
 
-	fields := []interface{}{
-		&s.a,
-		&s.b,
-		&s.c,
+	t.Parallel()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s := testData{
+				c: make([]byte, 1),
+			}
+
+			fields := []interface{}{
+				&s.a,
+				&s.b,
+				&s.c,
+			}
+
+			buf := bytes.NewBuffer(test.input)
+
+			err := Decode(buf, fields)
+			if err != nil {
+				if !test.wantFail {
+					t.Fatalf("Unexpected error: %s", err)
+				}
+
+				return
+			}
+
+			assert.Equal(t, test.expected, s)
+		})
 	}
-
-	buf := bytes.NewBuffer(input)
-	Decode(buf, fields)
-
-	expected := testData{
-		a: 3,
-		b: 100,
-		c: []byte{200},
-	}
-
-	assert.Equal(t, expected, s)
 }
