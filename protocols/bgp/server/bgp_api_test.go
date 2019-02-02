3496562a9cd85f0660ca00a96318fa5500a3d6a4
Oliver Herms <oliver.herms@exaring.de>
2019-02-02 17:29:46 +0100

    More tests

--- protocols/bgp/server/bgp_api_test.go
+++ protocols/bgp/server/bgp_api_test.go
@@ -6,8 +6,10 @@ import (
 
 	"github.com/bio-routing/bio-rd/protocols/bgp/api"
 	"github.com/bio-routing/bio-rd/protocols/bgp/packet"
+	"github.com/bio-routing/bio-rd/protocols/bgp/types"
 	"github.com/bio-routing/bio-rd/route"
 	routeapi "github.com/bio-routing/bio-rd/route/api"
+	"github.com/bio-routing/bio-rd/routingtable"
 	"github.com/bio-routing/bio-rd/routingtable/adjRIBIn"
 	"github.com/bio-routing/bio-rd/routingtable/filter"
 	"github.com/stretchr/testify/assert"
@@ -55,7 +57,7 @@ func TestDumpRIBIn(t *testing.T) {
 			wantFail: false,
 		},
 		{
-			name: "Test #2: One routes given",
+			name: "Test #2: One simple routes given",
 			apisrv: &BGPAPIServer{
 				srv: &bgpServer{
 					peers: &peerManager{
@@ -102,6 +104,93 @@ func TestDumpRIBIn(t *testing.T) {
 									ASPath:            []*routeapi.ASPathSegment{},
 									Communities:       []uint32{},
 									LargeCommunities:  []*routeapi.LargeCommunity{},
+									UnknownAttributes: []*routeapi.UnknownPathAttribute{},
+									ClusterList:       []uint32{},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantFail: false,
+		},
+		{
+			name: "Test #3: One complex routes given",
+			apisrv: &BGPAPIServer{
+				srv: &bgpServer{
+					peers: &peerManager{
+						peers: map[bnet.IP]*peer{
+							bnet.IPv4FromOctets(10, 0, 0, 0): {
+								fsms: []*FSM{
+									0: {
+										ipv4Unicast: &fsmAddressFamily{
+											adjRIBIn: adjRIBIn.New(filter.NewAcceptAllFilter(), routingtable.NewContributingASNs(), 0, 0, true),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			addRoutes: []*route.Route{
+				route.NewRoute(bnet.NewPfx(bnet.IPv4FromOctets(20, 0, 0, 0), 16), &route.Path{
+					Type: route.BGPPathType,
+					BGPPath: &route.BGPPath{
+						OriginatorID: 1,
+						NextHop:      bnet.IPv4FromOctets(100, 100, 100, 100),
+						Source:       bnet.IPv4FromOctets(100, 100, 100, 100),
+						ASPath: types.ASPath{
+							types.ASPathSegment{
+								Type: types.ASSequence,
+								ASNs: []uint32{15169, 3320},
+							},
+						},
+						Communities: []uint32{100, 200, 300},
+						LargeCommunities: []types.LargeCommunity{
+							{
+								GlobalAdministrator: 1,
+								DataPart1:           2,
+								DataPart2:           3,
+							},
+						},
+						LocalPref: 1000,
+						MED:       2000,
+					},
+				}),
+			},
+			req: &api.DumpRIBRequest{
+				Peer: bnet.IPv4FromOctets(10, 0, 0, 0).ToProto(),
+				Afi:  packet.IPv4AFI,
+				Safi: packet.UnicastSAFI,
+			},
+			expected: &api.DumpRIBResponse{
+				Routes: []*routeapi.Route{
+					{
+						Pfx: bnet.NewPfx(bnet.IPv4FromOctets(20, 0, 0, 0), 16).ToProto(),
+						Paths: []*routeapi.Path{
+							{
+								Type: routeapi.Path_BGP,
+								BGPPath: &routeapi.BGPPath{
+									OriginatorId: 1,
+									LocalPref:    1000,
+									MED:          2000,
+									NextHop:      bnet.IPv4FromOctets(100, 100, 100, 100).ToProto(),
+									Source:       bnet.IPv4FromOctets(100, 100, 100, 100).ToProto(),
+									ASPath: []*routeapi.ASPathSegment{
+										{
+											ASSequence: true,
+											ASNs:       []uint32{15169, 3320},
+										},
+									},
+									Communities: []uint32{100, 200, 300},
+									LargeCommunities: []*routeapi.LargeCommunity{
+										{
+											GlobalAdministrator: 1,
+											DataPart1:           2,
+											DataPart2:           3,
+										},
+									},
 									UnknownAttributes: []*routeapi.UnknownPathAttribute{},
 									ClusterList:       []uint32{},
 								},
