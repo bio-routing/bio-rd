87fa450cebe6b4d86b2b68ce2cab360fa5854ccb
Marcus Weiner <marcus.weiner@gmail.com>
2020-01-05 11:20:56 +0100

    Remove autogenerated tests for encoding

--- protocols/ospf/packetv3/fixtures/packets/base.go
+++
@@ -1,6 +0,0 @@
-package packets
-
-import "github.com/bio-routing/bio-rd/protocols/ospf/packetv3"
-
-// Packets by source file
-var Packets = make(map[string][]*packetv3.OSPFv3Message)
--- protocols/ospf/packetv3/packet_test.go
+++ protocols/ospf/packetv3/packet_test.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"testing"
 
 	ospf "github.com/bio-routing/bio-rd/protocols/ospf/packetv3"
 	"github.com/bio-routing/bio-rd/protocols/ospf/packetv3/fixtures"
-	"github.com/bio-routing/bio-rd/protocols/ospf/packetv3/fixtures/packets"
-	"github.com/stretchr/testify/assert"
 )
 
 var files = []string{
@@ -65,51 +62,6 @@ func testDecodeFile(t *testing.T, path string) {
 			if _, _, err := ospf.DeserializeOSPFv3Message(buf, src, dst); err != nil {
 				t.Error(err)
 			}
-		})
-		packetCount++
-	}
-}
-
-func TestEncode(t *testing.T) {
-	for _, path := range files {
-		t.Run(path, func(t *testing.T) {
-			testEncodeFile(t, dir+path)
-		})
-	}
-}
-
-func testEncodeFile(t *testing.T, path string) {
-	fmt.Printf("Testing on file: %s\n", path)
-	r, f := fixtures.PacketReader(t, path)
-	defer f.Close()
-
-	packets, ok := packets.Packets[filepath.Base(path)]
-	if !ok {
-		t.Errorf("Raw Go values not found for file %s", filepath.Base(path))
-	}
-
-	var packetCount int
-	for {
-		data, _, err := r.ReadPacketData()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		pl, src, dst, err := fixtures.Payload(data)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		t.Run(fmt.Sprintf("Packet_%03d", packetCount+1), func(t *testing.T) {
-			buf := &bytes.Buffer{}
-			msg := packets[packetCount]
-			msg.Serialize(buf, src, dst)
-			assert.Equal(t, buf.Bytes(), pl)
 		})
 		packetCount++
 	}
