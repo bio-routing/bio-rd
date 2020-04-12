bf222db967c09f9b99ef808f0dcd3303031fd965
Oliver Herms <oliver.peter.herms@gmail.com>
2020-04-13 00:42:50 +0200

    Refactoring

--- protocols/bgp/server/sockopt.go
+++ protocols/bgp/server/sockopt.go
@@ -7,21 +7,23 @@ import (
 )
 
 func setTTL(c net.Conn, ttl uint8) error {
+	// as c is an interface for testability reason we're checking here if the concrete type
+	// is a real TCP connection as only that supports setting a TTL
 	switch c.(type) {
 	case *tcp.Conn:
+		return c.(*tcp.Conn).SetTTL(ttl)
 	default:
 		return nil
 	}
-
-	return c.(*tcp.Conn).SetTTL(ttl)
 }
 
 func setDontRoute(c net.Conn) error {
+	// as c is an interface for testability reason we're checking here if the concrete type
+	// is a real TCP connection as only that supports setting SetDontRoute()
 	switch c.(type) {
 	case *tcp.Conn:
+		return c.(*tcp.Conn).SetDontRoute()
 	default:
 		return nil
 	}
-
-	return c.(*tcp.Conn).SetDontRoute()
 }
