59c91ef3b67f097963bbe5576deb96e5d0d28f09
Oliver Herms <oliver.herms@exaring.de>
2020-10-28 13:30:48 +0100

    Make linter happy

--- net/ethernet/mock_handler.go
+++ net/ethernet/mock_handler.go
@@ -6,13 +6,16 @@ import (
 	btesting "github.com/bio-routing/bio-rd/testing"
 )
 
+// MockHandler mocks an ethernet handler
 type MockHandler struct {
 }
 
+// NewMockHandler creates a new mock handler
 func NewMockHandler() HandlerInterface {
 	return &MockHandler{}
 }
 
+// NewConn creates a new mocked ethernet conn
 func (m *MockHandler) NewConn(dest MACAddr) net.Conn {
 	return &MockConn{
 		eth:      m,
@@ -26,14 +29,17 @@ func (m *MockHandler) RecvPacket() (pkt []byte, src MACAddr, err error) {
 	return nil, MACAddr{}, nil
 }
 
+// MCastJoin is here to fulfill an interface
 func (m *MockHandler) MCastJoin(addr MACAddr) error {
 	return nil
 }
 
+// GetMTU is here to fulfill an interface
 func (m *MockHandler) GetMTU() int {
 	return 1500
 }
 
+// SendPacket is here to fulfill an interface
 func (m *MockHandler) SendPacket(dst MACAddr, pkt []byte) error {
 	return nil
 }
