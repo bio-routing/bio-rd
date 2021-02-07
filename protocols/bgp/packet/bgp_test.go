941410d3c086c57cad7cccf170b9b4b5b771a81e
jayay <meine-adresse@live.de>
2021-02-07 10:06:35 +0100

    Add test for protocols/bgp/packet/bgp.go

---
+++ protocols/bgp/packet/bgp_test.go
@@ -0,0 +1,30 @@
+package packet
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAFIName(t *testing.T) {
+	afiIPv4 := AFIName(1)
+	assert.Equal(t, "IPv4", afiIPv4)
+
+	afiIPv6 := AFIName(2)
+	assert.Equal(t, "IPv6", afiIPv6)
+
+	afiUnknown := AFIName(0)
+	assert.Equal(t, "Unknown AFI", afiUnknown)
+}
+
+func TestBGPErrorError(t *testing.T) {
+	e := BGPError {
+		ErrorCode: 2,
+		ErrorSubCode: 3,
+		ErrorStr: "Unknown Error TestBGPErrorError",
+	}
+
+	actual := e.Error()
+	expected := "Unknown Error TestBGPErrorError"
+	assert.Equal(t, expected, actual)
+}
