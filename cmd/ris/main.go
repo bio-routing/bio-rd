56966a600dcb4e341472b4a75249e6984be7bd30
takt <oliver.peter.herms@gmail.com>
2022-08-18 16:17:10 +0200

    RIS: Add flag to allow sessions from anywhere (#359)
    Co-authored-by: Oliver Geiselhardt-Herms <ogeiselhardt-herms@cloudflare.com>

--- cmd/ris/main.go
+++ cmd/ris/main.go
@@ -25,22 +25,29 @@ var (
 	httpPort             = flag.Uint("http_port", 4320, "HTTP server port")
 	bmpListenAddr        = flag.String("bmp_addr", "0.0.0.0:30119", "BMP listen addr (set empty to disable listening)")
 	grpcKeepaliveMinTime = flag.Uint("grpc_keepalive_min_time", 1, "Minimum time (seconds) for a client to wait between GRPC keepalive pings")
-	configFilePath       = flag.String("config.file", "ris_config.yml", "Configuration file")
+	configFilePath       = flag.String("config.file", "", "Configuration file")
 	tcpKeepaliveInterval = flag.Uint("tcp-keepalive-interval", 1, "TCP keepalive interval (seconds)")
+	allowAny             = flag.Bool("allow.any", false, "Allow BMP sessions from anywhere")
 )
 
 func main() {
 	flag.Parse()
 
-	cfg, err := config.LoadConfig(*configFilePath)
-	if err != nil {
-		log.Errorf("Failed to load config: %v", err)
-		os.Exit(1)
+	cfg := &config.RISConfig{}
+	if *configFilePath != "" {
+		c, err := config.LoadConfig(*configFilePath)
+		if err != nil {
+			log.Fatalf("Failed to load config: %v", err)
+		}
+
+		cfg = c
 	}
 
 	b := server.NewServer(server.BMPServerConfig{
 		KeepalivePeriod: time.Duration(*tcpKeepaliveInterval) * time.Second,
+		AcceptAny:       *allowAny,
 	})
+
 	if *bmpListenAddr != "" {
 		go func() {
 			if err := b.Listen(*bmpListenAddr); err != nil {
@@ -55,8 +62,7 @@ func main() {
 	for _, r := range cfg.BMPServers {
 		ip := net.ParseIP(r.Address)
 		if ip == nil {
-			log.Errorf("unable to convert %q to net.IP", r.Address)
-			os.Exit(1)
+			log.Fatalf("unable to convert %q to net.IP", r.Address)
 		}
 		b.AddRouter(ip, r.Port, r.Passive, false)
 	}
