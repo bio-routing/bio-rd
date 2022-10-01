9f69fbb49674b090ac06bd7b6af59919e0112d7f
Maximilian Wilhelm <max@sdn.clinic>
2022-10-02 00:30:11 +0200

    Add more tests for Route related methods
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- route/route_test.go
+++ route/route_test.go
@@ -1,12 +1,14 @@
 package route
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	bnet "github.com/bio-routing/bio-rd/net"
 	"github.com/bio-routing/bio-rd/protocols/bgp/types"
+	"github.com/bio-routing/bio-rd/route/api"
 )
 
 func TestPathSelection(t *testing.T) {
@@ -149,6 +151,56 @@ func TestNewRoute(t *testing.T) {
 	}
 }
 
+func TestNewRouteAddPath(t *testing.T) {
+	tests := []struct {
+		name     string
+		pfx      *bnet.Prefix
+		paths    []*Path
+		expected *Route
+	}{
+		{
+			name: "BGP Path",
+			pfx:  bnet.NewPfx(bnet.IPv4FromOctets(10, 0, 0, 0), 8).Ptr(),
+			paths: []*Path{
+				{
+					Type:    BGPPathType,
+					BGPPath: &BGPPath{},
+				},
+				{
+					Type:    BGPPathType,
+					BGPPath: &BGPPath{},
+				},
+			},
+			expected: &Route{
+				pfx: bnet.NewPfx(bnet.IPv4FromOctets(10, 0, 0, 0), 8).Ptr(),
+				paths: []*Path{
+					{
+						Type:    BGPPathType,
+						BGPPath: &BGPPath{},
+					},
+					{
+						Type:    BGPPathType,
+						BGPPath: &BGPPath{},
+					},
+				},
+			},
+		},
+		{
+			name: "Empty Path",
+			pfx:  bnet.NewPfx(bnet.IPv4FromOctets(10, 0, 0, 0), 8).Ptr(),
+			expected: &Route{
+				pfx:   bnet.NewPfx(bnet.IPv4FromOctets(10, 0, 0, 0), 8).Ptr(),
+				paths: []*Path{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		res := NewRouteAddPath(test.pfx, test.paths)
+		assert.Equal(t, test.expected, res)
+	}
+}
+
 func TestPrefix(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -268,7 +320,146 @@ func TestAddPath(t *testing.T) {
 	}
 }
 
-func TestRouteRemovePath(t *testing.T) {
+func TestRemovePath(t *testing.T) {
+	tests := []struct {
+		name     string
+		route    *Route
+		remove   *Path
+		expected int
+	}{
+		{
+			name: "nil path to remove",
+			route: &Route{
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 100,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 200,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 300,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+				},
+			},
+			remove:   nil,
+			expected: 3,
+		},
+		{
+			name: "Remove middle",
+			route: &Route{
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 100,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 200,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 300,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+				},
+			},
+			remove: &Path{
+				Type: BGPPathType,
+				BGPPath: &BGPPath{
+					BGPPathA: &BGPPathA{
+						LocalPref: 200,
+						Source:    bnet.IPv4(0).Ptr(),
+						NextHop:   bnet.IPv4(0).Ptr(),
+					},
+				},
+			},
+			expected: 2,
+		},
+		{
+			name: "Remove non-existent",
+			route: &Route{
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 10,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 20,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+				},
+			},
+			remove: &Path{
+				Type: BGPPathType,
+				BGPPath: &BGPPath{
+					BGPPathA: &BGPPathA{
+						LocalPref: 50,
+						Source:    bnet.IPv4(0).Ptr(),
+						NextHop:   bnet.IPv4(0).Ptr(),
+					},
+				},
+			},
+			expected: 2,
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.route.RemovePath(test.remove))
+	}
+}
+
+func TestRemovePathInternal(t *testing.T) {
 	tests := []struct {
 		name     string
 		paths    []*Path
@@ -407,6 +598,118 @@ func TestRouteRemovePath(t *testing.T) {
 	}
 }
 
+func TestReplacePath(t *testing.T) {
+	tests := []struct {
+		name     string
+		route    *Route
+		old      *Path
+		new      *Path
+		expected error
+	}{
+		{
+			name: "Repalce first",
+			route: &Route{
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 100,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 200,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 300,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+				},
+			},
+			old: &Path{
+				Type: BGPPathType,
+				BGPPath: &BGPPath{
+					BGPPathA: &BGPPathA{
+						LocalPref: 100,
+						Source:    bnet.IPv4(0).Ptr(),
+						NextHop:   bnet.IPv4(0).Ptr(),
+					},
+				},
+			},
+			new: &Path{
+				Type: BGPPathType,
+				BGPPath: &BGPPath{
+					BGPPathA: &BGPPathA{
+						LocalPref: 123,
+						Source:    bnet.IPv4(0).Ptr(),
+						NextHop:   bnet.IPv4(0).Ptr(),
+					},
+				},
+			},
+			expected: nil,
+		},
+		{
+			name: "Replace non-existent",
+			route: &Route{
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 10,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							BGPPathA: &BGPPathA{
+								LocalPref: 20,
+								Source:    bnet.IPv4(0).Ptr(),
+								NextHop:   bnet.IPv4(0).Ptr(),
+							},
+						},
+					},
+				},
+			},
+			old: &Path{
+				Type: BGPPathType,
+				BGPPath: &BGPPath{
+					BGPPathA: &BGPPathA{
+						LocalPref: 50,
+						Source:    bnet.IPv4(0).Ptr(),
+						NextHop:   bnet.IPv4(0).Ptr(),
+					},
+				},
+			},
+			expected: fmt.Errorf("Path not found"),
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.route.ReplacePath(test.old, test.new))
+	}
+
+}
+
 func TestCopy(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -442,6 +745,39 @@ func TestCopy(t *testing.T) {
 	for _, test := range tests {
 		res := test.route.Copy()
 		assert.Equal(t, test.expected, res)
+	}
+}
+
+func TestPaths(t *testing.T) {
+	tests := []struct {
+		name     string
+		route    *Route
+		path     *Path
+		expected []*Path
+	}{
+		{
+			name:     "nil Route",
+			route:    nil,
+			expected: nil,
+		},
+		{
+			name: "nil Path",
+			route: &Route{
+				paths: nil,
+			},
+			expected: nil,
+		},
+		{
+			name: "with path",
+			route: &Route{
+				paths: []*Path{},
+			},
+			expected: []*Path{},
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.route.Paths())
 	}
 }
 
@@ -743,5 +1079,166 @@ func TestRouteIsBGPOriginatedBy(t *testing.T) {
 	for _, tc := range tests {
 		res := tc.r.IsBGPOriginatedBy(tc.origBy)
 		assert.Equal(t, tc.isOrig, res, tc.name)
+	}
+}
+
+func TestRouteToProto(t *testing.T) {
+	tests := []struct {
+		route  *Route
+		result *api.Route
+	}{
+		{
+			route: &Route{
+				pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr(),
+				paths: []*Path{
+					{
+						Type: StaticPathType,
+						StaticPath: &StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr(),
+						},
+					},
+					{
+						Type: StaticPathType,
+						StaticPath: &StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 1, 1).Ptr(),
+						},
+					},
+				},
+			},
+			result: &api.Route{
+				Pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr().ToProto(),
+				Paths: []*api.Path{
+					{
+						Type: api.Path_Static,
+						StaticPath: &api.StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr().ToProto(),
+						},
+					},
+					{
+						Type: api.Path_Static,
+						StaticPath: &api.StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 1, 1).Ptr().ToProto(),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		assert.Equal(t, tc.result, tc.route.ToProto())
+	}
+}
+
+func TestRouteFromProtoRoute(t *testing.T) {
+	tests := []struct {
+		name       string
+		protoRoute *api.Route
+		result     *Route
+	}{
+		{
+			name: "Static route",
+			protoRoute: &api.Route{
+				Pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr().ToProto(),
+				Paths: []*api.Path{
+					{
+						Type: api.Path_Static,
+						StaticPath: &api.StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr().ToProto(),
+						},
+					},
+					{
+						Type: api.Path_Static,
+						StaticPath: &api.StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 1, 1).Ptr().ToProto(),
+						},
+					},
+				},
+			},
+			result: &Route{
+				pfx:       bnet.NewPfx(bnet.IPv4(0), 0).Ptr(),
+				ecmpPaths: 0,
+				paths: []*Path{
+					{
+						Type: StaticPathType,
+						StaticPath: &StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr(),
+						},
+					},
+					{
+						Type: StaticPathType,
+						StaticPath: &StaticPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 1, 1).Ptr(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Bare BGP route",
+			protoRoute: &api.Route{
+				Pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr().ToProto(),
+				Paths: []*api.Path{
+					{
+						Type: api.Path_BGP,
+						BgpPath: &api.BGPPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr().ToProto(),
+							Source:  bnet.IPv4FromOctets(192, 168, 0, 2).Ptr().ToProto(),
+						},
+					},
+				},
+			},
+
+			result: &Route{
+				pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr(),
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							ASPath: &types.ASPath{},
+							BGPPathA: &BGPPathA{
+								NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr(),
+								Source:  bnet.IPv4FromOctets(192, 168, 0, 2).Ptr(),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Bare BGP route",
+			protoRoute: &api.Route{
+				Pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr().ToProto(),
+				Paths: []*api.Path{
+					{
+						Type: api.Path_BGP,
+						BgpPath: &api.BGPPath{
+							NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr().ToProto(),
+							Source:  bnet.IPv4FromOctets(192, 168, 0, 2).Ptr().ToProto(),
+						},
+					},
+				},
+			},
+
+			result: &Route{
+				pfx: bnet.NewPfx(bnet.IPv4(0), 0).Ptr(),
+				paths: []*Path{
+					{
+						Type: BGPPathType,
+						BGPPath: &BGPPath{
+							ASPath: &types.ASPath{},
+							BGPPathA: &BGPPathA{
+								NextHop: bnet.IPv4FromOctets(192, 168, 0, 1).Ptr(),
+								Source:  bnet.IPv4FromOctets(192, 168, 0, 2).Ptr(),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		assert.Equal(t, tc.result, RouteFromProtoRoute(tc.protoRoute, false))
 	}
 }
