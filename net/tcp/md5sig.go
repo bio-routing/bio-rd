8430b9e2c05abc1a5bf3e6cfe24083fe8ae5176b
Tobias Klauser <tklauser@distanz.ch>
2023-03-07 09:26:59 +0100

    net/tcp: use SetsockoptTCPMD5Sig from golang.org/x/sys/unix
    Use the TCPMD5Sig type and its corresponding SetsockoptTCPMD5Sig helper
    added in x/sys v0.6.0 to set the TCP MD5 sig on a socket.

--- net/tcp/md5sig.go
+++ net/tcp/md5sig.go
@@ -2,53 +2,35 @@ package tcp
 
 import (
 	"net"
-	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
 const (
-	tcpMD5SIG           = 14 // (RFC2385)
-	tcpMD5SIGMaxKeyLen  = 80
 	tcpMD5SIGFlagPrefix = 0
 )
 
-type tcpMD5sig struct {
-	ssFamily  uint16
-	ss        [126]byte
-	flags     uint8
-	prefixLen uint8
-	keylen    uint16
-	ifIndex   uint32
-	key       [tcpMD5SIGMaxKeyLen]byte
-}
-
-func buildTCPMD5Sig(addr net.IP, key string) tcpMD5sig {
-	family := unix.AF_INET
-	if addr.To4() == nil {
-		family = unix.AF_INET6
+func buildTCPMD5Sig(addr net.IP, key string) *unix.TCPMD5Sig {
+	t := &unix.TCPMD5Sig{
+		Flags:     tcpMD5SIGFlagPrefix,
+		Prefixlen: 0,
+		Keylen:    uint16(len(key)),
 	}
 
-	t := tcpMD5sig{
-		ssFamily:  uint16(family),
-		flags:     tcpMD5SIGFlagPrefix,
-		prefixLen: 0,
-		keylen:    uint16(len(key)),
-	}
-
-	if family == unix.AF_INET {
-		copy(t.ss[2:], addr.To4())
+	if addr.To4() != nil {
+		t.Addr.Family = unix.AF_INET
+		copy(t.Addr.Data[2:], addr.To4())
 	} else {
-		copy(t.ss[6:], addr.To16())
+		t.Addr.Family = unix.AF_INET6
+		copy(t.Addr.Data[6:], addr.To16())
 	}
 
-	copy(t.key[0:], key)
+	copy(t.Key[0:], key)
 
 	return t
 }
 
 func setTCPMD5Option(fd int, addr net.IP, md5secret string) error {
 	sig := buildTCPMD5Sig(addr, md5secret)
-	b := *(*[unsafe.Sizeof(sig)]byte)(unsafe.Pointer(&sig))
-	return unix.SetsockoptString(fd, unix.IPPROTO_TCP, tcpMD5SIG, string(b[:]))
+	return unix.SetsockoptTCPMD5Sig(fd, unix.IPPROTO_TCP, unix.TCP_MD5SIG, sig)
 }
